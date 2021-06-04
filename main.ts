52b540e514a4cae7ab4ff57d45df64b4289ff84e
ryoha000 <the7uya@gmail.com>
2021-06-05 06:28:15 +0900

    add handle api

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+import { getDatas } from './lib/scrape.ts'
+import { searchData } from './lib/data.ts'
+
 const BASE_URL = "https://raw.githubusercontent.com/ryoha000/lovesis/master/public"
 
 const handleRequest = async (request: Request) => {
@@ -6,7 +9,22 @@ const handleRequest = async (request: Request) => {
       const url = new URL(request.url)
       const pathname = url.pathname
       let res: ArrayBuffer
-      if (pathname && pathname !== '/') {
+      if (pathname.startsWith('/api')) {
+        if (pathname === '/api/search') {
+          const queryTitle = url.searchParams.get('title')
+          if (!queryTitle) {
+            return new Response(null, { status: 400 })
+          }
+          const data = searchData(queryTitle, datas)
+          if (!data) {
+            return new Response(null, { status: 404 })
+          }
+          return new Response(JSON.stringify(data), {
+            headers: { "content-type": "application/json" }
+          });
+        }
+        return new Response(null, { status: 405 })
+      } else if (pathname && pathname !== '/') {
         res = await (await fetch(`${BASE_URL}/${pathname}`)).arrayBuffer()
       } else {
         res = await (await fetch(`${BASE_URL}/index.html`)).arrayBuffer()
@@ -20,6 +38,11 @@ const handleRequest = async (request: Request) => {
     }
   }
 }
+
+let datas = await getDatas()
+setInterval(async () => {
+  datas = await getDatas()
+})
 
 const isRequestEvent = (e: Event): e is RequestEvent => {
   return 'request' in e && 'respondWith' in e
