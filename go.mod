3a3aca18cc97b49e8db9dfa174a219d7001489ea
ryoha000 <the7uya@gmail.com>
2021-06-08 06:38:37 +0900

    add sumple code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ryoha000/lovesis
+
+go 1.16
+
+require github.com/mattn/go-oci8 v0.1.1
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+    "database/sql"
+    "fmt"
+    "os"
+    _ "github.com/mattn/go-oci8"
+)
+
+func execSQL(db *sql.DB, sql string) bool{
+    _,err := db.Exec(sql)
+    if err != nil {
+        fmt.Println(err)
+        return false
+    } else {
+        return true
+    }
+}
 
 func main() {
-	fmt.Println("Hello world")
+    db, err := sql.Open("oci8", os.Args[1])
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer db.Close()
+    if !execSQL(db,"create table HOGE (NUM number(2),WORD varchar2(10))"){
+        return
+    }
+    if !execSQL(db,"insert into HOGE values (1,'One')"){
+        return
+    }
+    if !execSQL(db,"insert into HOGE values (2,'Two')"){
+        return
+    }
+    if !execSQL(db,"insert into HOGE values (3,'Three')"){
+        return
+    }
+
+    rows ,err := db.Query("select NUM,WORD  from HOGE order by NUM")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer rows.Close()
+    for rows.Next(){
+        var num int 
+        var word string
+
+        rows.Scan(&num, &word)
+        fmt.Println(num, word)
+    }
+    _,err = db.Exec("drop table HOGE")
 }
